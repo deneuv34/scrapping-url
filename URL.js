e18b07d2aede6388d94d6a9f6bca0134d930fe2f
Pholenk <pholenkadi17@gmail.com>
2017-01-18 19:05:03 +0700

    Fixed minor bug

--- URL.js
+++ URL.js
@@ -9,26 +9,22 @@ function Dummy(alamatUrl)
   this.url = alamatUrl
 }
 Dummy.prototype.scrapping = function(data){
-  console.log(this.url);
   nama_file = url.parse(this.url, true).host;
   fs.writeFile('' + nama_file +'.txt', data , (err) => {
     if (err) throw err;
   });
-  return console.log('It\'s saved!');
 }
 Dummy.prototype.save = function(){
   var that = this;
   scrap(this.url, function(html, $) {
       var data = $('html').text().trim();
-      console.log(that.scrapping(data));
+      that.scrapping(data);
     });
 }
 var web = [];
 var i = 0;
 for (var iterasi in json) {
-  console.log(i);
     i++;
-    console.log('iteration', iterasi);
     web[i] = new Dummy(json[iterasi]);
     web[i].save();
 }
