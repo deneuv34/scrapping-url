30a51bfa9b092ea2a97b60c8793fa4b4b91fefda
Pholenk <pholenkadi17@gmail.com>
2017-01-18 18:33:42 +0700

    this file is very clear

--- URL.js
+++ URL.js
@@ -2,29 +2,33 @@ var url = require('url');
 var scrap = require('scrap');
 var $ = require('cheerio');
 var fs = require('fs');
-console.log($);
+var json = require('./url.json');
+
 function Dummy(alamatUrl)
 {
   this.url = alamatUrl
 }
 Dummy.prototype.scrapping = function(data){
-
+  console.log(this.url);
   nama_file = url.parse(this.url, true).host;
   fs.writeFile('' + nama_file +'.txt', data , (err) => {
     if (err) throw err;
   });
   return console.log('It\'s saved!');
 }
-
-
-var web1 = new Dummy('http://refactory.id');
-scrap(web1.url, function(html, $) {
-var data = $('html').text().trim();
-console.log(web1.scrapping(data));
-});
-
-var web2 = new Dummy('https://idntimes.com');
-scrap(web1.url, function(html, $) {
-var data = $('html').text().trim();
-console.log(web1.scrapping(data));
-});
+Dummy.prototype.save = function(){
+  var that = this;
+  scrap(this.url, function(html, $) {
+      var data = $('html').text().trim();
+      console.log(that.scrapping(data));
+    });
+}
+var web = [];
+var i = 0;
+for (var iterasi in json) {
+  console.log(i);
+    i++;
+    console.log('iteration', iterasi);
+    web[i] = new Dummy(json[iterasi]);
+    web[i].save();
+}
